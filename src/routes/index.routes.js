468c975384509a8c127cb4e01380541390da11fc
Sarahribeiro5 <158165830+Sarahribeiro5@users.noreply.github.com>
2025-02-06 11:37:17 -0300

    feat: finalizacao revisao

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
+import studentsRouter from "./students.routes.js";
+import usersRouter from "./users.routes.js";
 
 const routes = Router();
 
-// Rota raiz para teste
 routes.get("/", (req, res) => {
-  return res.status(200).json({ message: "Vai Corinthians!" });
+  return res.status(200).send({ message: "Vai Corinthians!" });
 });
 
-export default routes;
+router.use("/students", studentsRouter);
+router.use("/users", usersRouter)
+
+export { router };
---
+++ src/routes/students.routes.js
@@ -0,0 +1,26 @@
+import { Router } from "express";
+
+import {
+    getStudents,
+    getStudentsById,
+    createStudents,
+    updateStudent,
+    deleteStudent,
+} from "../controllers/students.controller.js";
+
+const studentsRouter = Router();
+
+studentsRouter.get("/", getStudents);
+studentsRouter.get("/:id", getStudentById);
+studentsRouter.post("/", createStudent);
+studentsRouter.put("/:id", updateStudent);
+studentsRouter.delete("/:id", deleteStudent);
+
+export default studentsRouter; 
+
+
+
+
+
+
+ 
